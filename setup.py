8d872e066b51c5ba77fde9ff1cc44145909fe46c
anakazh <59440860+anakazh@users.noreply.github.com>
2021-12-09 01:45:10 +0100

    bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ pkg_name = 'fincnn'
 
 setuptools.setup(
     name=pkg_name,
-    version="0.6",
+    version="0.7",
     description="Technical analysis CNN models for stock return prediction",
     long_description=long_description,
     long_description_content_type="text/markdown",
