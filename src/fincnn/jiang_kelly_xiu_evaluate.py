8855554385f3c8aa6bc7f582b6623268b68e2084
anakazh <59440860+anakazh@users.noreply.github.com>
2022-01-24 11:31:29 +0100

    skip 5-day model

--- src/fincnn/jiang_kelly_xiu_evaluate.py
+++ src/fincnn/jiang_kelly_xiu_evaluate.py
@@ -5,6 +5,8 @@ if __name__ == '__main__':
 
     for return_horizon in RETURN_HORIZONS:
         for CNN, model_name in zip([CNN_5, CNN_20, CNN_60], ['jkx_CNN_5', 'jkx_CNN_20', 'jkx_CNN_60']):
+            if model_name == 'jkx_CNN_5':
+                continue
             name = f'{model_name}_{return_horizon}'
             cnn = CNN(return_horizon=return_horizon, name=name)
             cnn.evaluate()
