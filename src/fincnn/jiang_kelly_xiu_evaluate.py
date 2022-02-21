79f91d92b11bebcaddaf7b29201fc51789f99082
anakazh <59440860+anakazh@users.noreply.github.com>
2022-02-21 13:30:50 +0100

    evaluate 20 and 60-day models

--- src/fincnn/jiang_kelly_xiu_evaluate.py
+++ src/fincnn/jiang_kelly_xiu_evaluate.py
@@ -4,7 +4,7 @@ from fincnn.config.generate_datasets_config import RETURN_HORIZONS
 if __name__ == '__main__':
 
     for CNN, model_name in zip([CNN_5, CNN_20, CNN_60], ['jkx_CNN_5', 'jkx_CNN_20', 'jkx_CNN_60']):
-        if model_name in ['jkx_CNN_20', 'jkx_CNN_60']:
+        if model_name == 'jkx_CNN_5':
             continue
         for return_horizon in RETURN_HORIZONS:
             name = f'{model_name}_{return_horizon}'
